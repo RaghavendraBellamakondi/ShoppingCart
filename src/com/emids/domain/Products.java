7e4255f7f082c43eaeabc3593d96f8b8953b2634
LaxmiRathore <rathorelaxmi.rathore@gmail.com>
2017-10-16 16:07:34 +0530

    this is my sixth commit

--- src/com/emids/domain/Products.java
+++ src/com/emids/domain/Products.java
@@ -12,16 +12,16 @@ public class Products {
 
 	public static void getItem() {
 		if (book != null) {
-			System.out.println(book.name + "  by  " + book.author);
+			System.out.println(book.name + "  by  " + book.author + "\t" + "Rs: " + book.price);
 		}
 		if (book1 != null) {
-			System.out.println(book1.name + " by " + book1.author);
+			System.out.println(book1.name + " by " + book1.author + "\t" + "Rs: " + book1.price);
 		}
 		if (book2 != null) {
-			System.out.println(book2.name + "  by " + book2.author);
+			System.out.println(book2.name + "  by " + book2.author + "\t" + "Rs: " + book2.price);
 		}
 		if (book3 != null) {
-			System.out.println(book3.name + "  by " + book3.author);
+			System.out.println(book3.name + "  by " + book3.author + "\t" + "Rs: " + book3.price);
 		}
 
 	}
--- src/com/emids/service/Inventory.java
+++ src/com/emids/service/Inventory.java
@@ -3,14 +3,6 @@ package com.emids.service;
 import com.emids.domain.Products;
 
 public class Inventory {
-	boolean findBooks(String name) {
-		if (name == Products.book.name)
-			return true;
-		else if (name == Products.book1.name)
-			return true;
-		else
-			return false;
-	}
 
 	void search() {
 		Inventory.show();
